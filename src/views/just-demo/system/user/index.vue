1604f3b3f3eed3998a0b8c17ad9ed7427defcc05
Yuwx <>
2025-04-15 21:58:30 +0800

    feat: just-demo添加user的DeptTree

--- src/views/just-demo/system/user/index.vue
+++ src/views/just-demo/system/user/index.vue
@@ -1,7 +1,16 @@
 <template>
-  <div class=""> haha </div>
+  <SplitPanel>
+    <template #left-content>
+      <DeptTree />
+    </template>
+    <template #right-content>
+      <div>right</div>
+    </template>
+  </SplitPanel>
 </template>
 
-<script setup name="" lang="ts"></script>
+<script setup name="" lang="ts">
+  import DeptTree from './components/DeptTree.vue';
+</script>
 
 <style lang="scss" scoped></style>
